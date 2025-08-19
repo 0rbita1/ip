049cd1bf9fcb2b25a4e2ebc73e6ad396b1144a30
0ribta1 <junxiang5563@gmail.com>
2025-08-19 16:19:14 +0800

    Add Task class, add marking functionality (Level-3)

--- src/main/java/Keeka.java
+++ src/main/java/Keeka.java
@@ -1,16 +1,12 @@
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Keeka {
 
     public static void main(String[] args) {
 
         String name = "Keeka";
-        Scanner scanner = new Scanner(System.in);
-        String userInput = "";
-        ArrayList<String> textList = new ArrayList<>();
 
         // Greeting
         System.out.println("_________________________________________________");
@@ -18,22 +14,7 @@ public class Keeka {
         System.out.println("What can I do for you?");
         System.out.println("_________________________________________________");
 
-        // Append to list
-        userInput = scanner.nextLine();
-        while (!Objects.equals(userInput, "bye")) {
-
-            if (Objects.equals(userInput, "list")) {
-                listReader(textList);
-                userInput = scanner.nextLine();
-                continue;
-            }
-
-            textList.add(userInput);
-            System.out.println("_________________________________________________");
-            System.out.println("Text successfully added: " + userInput);
-            System.out.println("_________________________________________________");
-            userInput = scanner.nextLine();
-        }
+        inputHandler();
 
         // Exit
         System.out.println("_________________________________________________");
@@ -42,10 +23,52 @@ public class Keeka {
 
     }
 
-    // Text list printer
-    private static void listReader(ArrayList<String> textList) {
+    public static void inputHandler() {
 
-        if (textList.isEmpty()) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        ArrayList<Task> taskList = new ArrayList<>();
+        userInput = scanner.nextLine();
+
+        while (!Objects.equals(userInput, "bye")) {
+
+            if (Objects.equals(userInput, "list")) {
+                listPrinter(taskList);
+                userInput = scanner.nextLine();
+                continue;
+            }
+
+            try {
+                String[] markingArray = userInput.split(" ");
+                String isMarkString = markingArray[0];
+                int indexNumber = Integer.parseInt(markingArray[1]);
+
+                if (Objects.equals(isMarkString, "mark")) {
+                    listMarker(taskList, indexNumber);
+                }
+
+                if (Objects.equals(isMarkString, "unmark")) {
+                    listUnmarker(taskList, indexNumber);
+                }
+
+            } catch (Exception e){
+                Task newTask = new Task(userInput, false);
+                taskList.add(newTask);
+                System.out.println("_________________________________________________");
+                System.out.println("Task successfully added: " + newTask.getDescription());
+                System.out.println("_________________________________________________");
+            } finally {
+                userInput = scanner.nextLine();
+            }
+
+        }
+
+    }
+
+    // Text list printer
+    private static void listPrinter(ArrayList<Task> taskList) {
+
+        if (taskList.isEmpty()) {
             System.out.println("_________________________________________________");
             System.out.println("List is empty! Add texts to display");
             System.out.println("_________________________________________________");
@@ -53,14 +76,57 @@ public class Keeka {
         }
 
         System.out.println("_________________________________________________");
+        System.out.println("Displaying list items");
 
-        for (int i = 0; i < textList.size(); i++) {
-            String currentText = textList.get(i);
-            System.out.println((i + 1) + ". " + currentText);
+        for (int i = 0; i < taskList.size(); i++) {
+            Task currentText = taskList.get(i);
+            System.out.println((i + 1) + ". " + currentText.toString());
         }
 
         System.out.println("_________________________________________________");
 
     }
+
+    // Marks items in list, updates status in list
+    private static void listMarker(ArrayList<Task> taskList, int index) {
+
+        if (index < 1 || index > taskList.size()) {
+            System.out.println("_________________________________________________");
+            System.out.println("Invalid index number!");
+            System.out.println("_________________________________________________");
+            return;
+        }
+
+        Task desiredTask = taskList.get(index - 1);
+        desiredTask.setDone();
+        taskList.set(index - 1, desiredTask);
+
+        System.out.println("_________________________________________________");
+        System.out.println("Task successfully marked as done:");
+        System.out.println(desiredTask);
+        System.out.println("_________________________________________________");
+
+    }
+
+    private static void listUnmarker(ArrayList<Task> taskList, int index) {
+
+        if (index < 1 || index > taskList.size()) {
+            System.out.println("_________________________________________________");
+            System.out.println("Invalid index number!");
+            System.out.println("_________________________________________________");
+            return;
+        }
+
+        Task desiredTask = taskList.get(index - 1);
+        desiredTask.setNotDone();
+        taskList.set(index - 1, desiredTask);
+
+        System.out.println("_________________________________________________");
+        System.out.println("Task successfully marked as NOT done:");
+        System.out.println(desiredTask);
+        System.out.println("_________________________________________________");
+
+    }
+
 }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] "  + this.description;
+    }
+}
