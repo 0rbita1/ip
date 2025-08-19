055da091ef2ade6ad59cf3c28ef73bea3ab29f5a
0ribta1 <junxiang5563@gmail.com>
2025-08-19 13:10:58 +0800

    Rename Duke to Keeka (Level-0)

---
+++ src/main/java/Keeka.java
@@ -0,0 +1,19 @@
+public class Keeka {
+
+    public static void main(String[] args) {
+
+        String name = "Keeka";
+
+        // Greeting
+        System.out.println("_________________________________________________");
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+
+        // Exit
+        System.out.println("_________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________________");
+
+    }
+}
+
