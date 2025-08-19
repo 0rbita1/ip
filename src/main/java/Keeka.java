dfd6b348eef57acdec3b94f183ea50b52a7ca72e
0ribta1 <junxiang5563@gmail.com>
2025-08-19 13:23:04 +0800

    Add echoing of user commands (Level-1)

--- src/main/java/Keeka.java
+++ src/main/java/Keeka.java
@@ -1,13 +1,28 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Keeka {
 
     public static void main(String[] args) {
 
         String name = "Keeka";
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
 
         // Greeting
         System.out.println("_________________________________________________");
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+        System.out.println("_________________________________________________");
+
+        // Echo
+        userInput = scanner.nextLine();
+        while (!Objects.equals(userInput, "bye")) {
+            System.out.println("_________________________________________________");
+            System.out.println(userInput);
+            System.out.println("_________________________________________________");
+            userInput = scanner.nextLine();
+        }
 
         // Exit
         System.out.println("_________________________________________________");
