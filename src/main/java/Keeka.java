9174af034168fdc7c74a7efeba81fae8d7869e8a
0ribta1 <junxiang5563@gmail.com>
2025-08-20 08:59:04 +0800

    Refactored routing logic to be in inputHandler method

--- src/main/java/Keeka.java
+++ src/main/java/Keeka.java
@@ -20,14 +20,12 @@ public class Keeka {
         System.out.println("_________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_________________________________________________");
-
     }
 
-    public static void inputHandler() {
+    // Attempts to parse first word to call the appropriate method
+    private static void inputHandler() {
 
         Scanner scanner = new Scanner(System.in);
-        boolean isTask = false;
-        boolean isMark = false;
         String userInput = "";
         ArrayList<Task> taskList = new ArrayList<>();
         userInput = scanner.nextLine();
@@ -42,10 +40,16 @@ public class Keeka {
 
             try {
 
-                isTask = taskInterpreter(userInput, taskList);
-                isMark = markingInterpreter(userInput, taskList);
+                String[] parsedInput = userInput.split(" ", 2);
+                String firstWord = parsedInput[0];
 
-                if (!isTask && !isMark) {
+                if (Objects.equals(firstWord, "todo") || (Objects.equals(firstWord, "deadline")) || (Objects.equals(firstWord, "event"))) {
+                    taskInterpreter(userInput, taskList);
+                } else if (Objects.equals(firstWord, "mark") || (Objects.equals(firstWord, "unmark"))) {
+                    markingInterpreter(userInput, taskList);
+                } else if (Objects.equals(firstWord, "delete")) {
+                    deleteInterpreter(userInput, taskList);
+                } else {
                     throw new Exception("Invalid command!");
                 }
 
@@ -59,50 +63,46 @@ public class Keeka {
         }
     }
 
-    public static boolean taskInterpreter(String userInput, ArrayList<Task> taskList) throws InvalidTaskException {
+    // Creates the appropriate task and adds to list
+    private static void taskInterpreter(String userInput, ArrayList<Task> taskList) throws InvalidTaskException {
 
         try {
             String[] parsedInput = userInput.split(" ", 2);
             String firstElement = parsedInput[0];
             String description = parsedInput[1];
 
-            if (Objects.equals(firstElement, "todo") || (Objects.equals(firstElement, "deadline")) || (Objects.equals(firstElement, "event"))) {
-
-                switch (firstElement) {
-                    case "todo" -> {
-                        ToDo newToDo = new ToDo(description, false);
-                        taskList.add(newToDo);
-                    }
-                    case "deadline" -> {
-                        String[] parsedDescription = description.split(" /by ");
-                        String deadlineDescription = parsedDescription[0];
-                        String deadlineDate = parsedDescription[1];
-                        Deadline newDeadline = new Deadline(deadlineDescription, false, deadlineDate);
-                        taskList.add(newDeadline);
-                    }
-                    case "event" -> {
-                        String[] parsedDescription = description.split(" /from ");
-                        String eventDescription = parsedDescription[0];
-                        String[] parsedDuration = parsedDescription[1].split(" /to ");
-                        String eventStart = parsedDuration[0];
-                        String eventEnd = parsedDuration[1];
-                        Event newEvent = new Event(eventDescription, false, eventStart, eventEnd);
-                        taskList.add(newEvent);
-                    }
+            switch (firstElement) {
+                case "todo" -> {
+                    ToDo newToDo = new ToDo(description, false);
+                    taskList.add(newToDo);
                 }
-                successfulTaskAddPrinter(taskList);
-                return true;
-            } else {
-                return false;
+                case "deadline" -> {
+                    String[] parsedDescription = description.split(" /by ");
+                    String deadlineDescription = parsedDescription[0];
+                    String deadlineDate = parsedDescription[1];
+                    Deadline newDeadline = new Deadline(deadlineDescription, false, deadlineDate);
+                    taskList.add(newDeadline);
+                }
+                case "event" -> {
+                    String[] parsedDescription = description.split(" /from ");
+                    String eventDescription = parsedDescription[0];
+                    String[] parsedDuration = parsedDescription[1].split(" /to ");
+                    String eventStart = parsedDuration[0];
+                    String eventEnd = parsedDuration[1];
+                    Event newEvent = new Event(eventDescription, false, eventStart, eventEnd);
+                    taskList.add(newEvent);
+                }
             }
 
-        } catch (Exception e) {
-            return false;
-        }
+            successfulTaskAddPrinter(taskList);
 
+        } catch (Exception e) {
+            throw new InvalidTaskException("Invalid task invocation!");
+        }
     }
 
-    public static void successfulTaskAddPrinter(ArrayList<Task> taskList) {
+
+    private static void successfulTaskAddPrinter(ArrayList<Task> taskList) {
         System.out.println("_________________________________________________");
         System.out.println("Task successfully added: ");
         System.out.println(taskList.get(taskList.size() - 1));
@@ -110,31 +110,55 @@ public class Keeka {
         System.out.println("_________________________________________________");
     }
 
-    public static boolean markingInterpreter(String userInput, ArrayList<Task> taskList) throws InvalidMarkingException {
+    // Parses first word to determine which marking method to call
+    private static void markingInterpreter(String userInput, ArrayList<Task> taskList) throws InvalidMarkingException {
 
         try {
             String[] markingArray = userInput.split(" ");
             String isMarkString = markingArray[0];
             int indexNumber = Integer.parseInt(markingArray[1]);
 
-            if ((Objects.equals(isMarkString, "mark")) || (Objects.equals(isMarkString, "unmark"))) {
-                switch (isMarkString) {
-                    case "mark" -> {
-                        listMarker(taskList, indexNumber);
-                    }
-                    case "unmark" -> {
-                        listUnmarker(taskList, indexNumber);
-                    }
-                }
-                return true;
-            } else {
-                return false;
+            switch (isMarkString) {
+                case "mark" -> listMarker(taskList, indexNumber);
+                case "unmark" -> listUnmarker(taskList, indexNumber);
             }
 
         } catch (Exception e) {
-            return false;
+            throw new InvalidMarkingException("Invalid marking invocation!");
+        }
+    }
+
+    // Parses input to determine index at which item is to be deleted
+    private static void deleteInterpreter(String userInput, ArrayList<Task> taskList) throws Exception {
+
+        try {
+            String[] deletionArray = userInput.split(" ");
+            String deleteString = deletionArray[0];
+            int indexNumber = Integer.parseInt(deletionArray[1]);
+            listDeleter(taskList, indexNumber);
+
+        } catch (Exception e) {
+            throw new Exception("Invalid deletion invocation!");
+        }
+    }
+
+    // Deletes item at specified index (1-indexed)
+    private static void listDeleter(ArrayList<Task> taskList, int index) {
+        if (index < 1 || index > taskList.size()) {
+            System.out.println("_________________________________________________");
+            System.out.println("Invalid index number! Use an integer within the range of the size of the list");
+            System.out.println("_________________________________________________");
+            return;
         }
 
+        Task desiredTask = taskList.get(index - 1);
+        taskList.remove(index);
+
+        System.out.println("_________________________________________________");
+        System.out.println("Task successfully deleted:");
+        System.out.println(desiredTask);
+        System.out.println("Task counter: " + taskList.size());
+        System.out.println("_________________________________________________");
     }
 
     // Text list printer
@@ -142,7 +166,7 @@ public class Keeka {
 
         if (taskList.isEmpty()) {
             System.out.println("_________________________________________________");
-            System.out.println("List is empty! Add texts to display");
+            System.out.println("List is empty! Add tasks to display");
             System.out.println("_________________________________________________");
             return;
         }
@@ -156,7 +180,6 @@ public class Keeka {
         }
 
         System.out.println("_________________________________________________");
-
     }
 
     // Marks items in list, updates status in list
@@ -164,7 +187,7 @@ public class Keeka {
 
         if (index < 1 || index > taskList.size()) {
             System.out.println("_________________________________________________");
-            System.out.println("Invalid index number!");
+            System.out.println("Invalid index number! Use an integer within the range of the size of the list");
             System.out.println("_________________________________________________");
             return;
         }
@@ -177,7 +200,6 @@ public class Keeka {
         System.out.println("Task successfully marked as done:");
         System.out.println(desiredTask);
         System.out.println("_________________________________________________");
-
     }
 
     // Unmarks items in list, updates status in list
@@ -185,7 +207,7 @@ public class Keeka {
 
         if (index < 1 || index > taskList.size()) {
             System.out.println("_________________________________________________");
-            System.out.println("Invalid index number!");
+            System.out.println("Invalid index number! Use an integer within the range of the size of the list");
             System.out.println("_________________________________________________");
             return;
         }
@@ -198,7 +220,6 @@ public class Keeka {
         System.out.println("Task successfully marked as NOT done:");
         System.out.println(desiredTask);
         System.out.println("_________________________________________________");
-
     }
 }
 
